8a4b2a22239675b3c8d9bba24e442deb150f4c6d
Asher McPherson <mcphersonasher@gmail.com>
2026-01-18 16:23:08 -0600

    Remove duplicate AddAccountRequest model

--- Models/AddAccountRequest.cs
+++
@@ -1,7 +0,0 @@
-using AltShare.Models;
-
-public class AddAccountRequest
-{
-    public string encryptedData { get; set; }
-    //public List<DecryptedSharedAccount> sharedAccounts { get; set; }
-} 
